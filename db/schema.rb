d69ae1a70f688c8be2a88ceba484ce5f55fa29f0
Satoru Nakata <nakata0705@gmail.com>
2018-02-05 21:42:35 +0100

    Change database to postgresql even for development environment.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180204223940) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sources", force: :cascade do |t|
     t.string "title", null: false
     t.string "link"
