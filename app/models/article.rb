c096294341e3785e17346af924e337f26f3fe950
Satoru Nakata <nakata0705@gmail.com>
2018-02-06 22:08:17 +0100

    Define translation action for the model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,51 @@
 class Article < ApplicationRecord
+    def bind
+        binding
+    end
+
+    def translate_to_en
+        if self.title_en == nil
+            if self.title_lang == 'en'
+                self.title_en = self.title
+            else
+                self.title_en = EasyTranslate.translate(self.title, from: self.title_lang, to: 'en', model: 'nmt', key: 'AIzaSyADjgRuNoE610_YLwW8P2_rKp08eEPDqFo')
+            end
+            self.save
+        end
+    end
+
+    def translate_from_en(to)
+        begin
+            if self.title_en == nil
+                raise "English translation doesn't exist"
+            end
+
+            if to == nil
+                raise "No target language"
+            end
+            
+            to.downcase!
+            to_google = nil
+            case to
+            when 'zh-cn' then
+                to = 'zh_cn'
+                to_google = 'zh-CN'
+            when 'zh-tw' then
+                to = 'zh_tw'
+                to_google = 'zh-TW'
+            else
+                to_google = to
+            end
+            
+            if self.attribute_names.index("title_#{to}") == nil
+                raise "Invalid target language #{to}"
+            end
+
+            if self["title_#{to}"] == nil
+                self["title_#{to}"] = EasyTranslate.translate(self.title_en, from: 'en', to: to_google, model: 'nmt', key: 'AIzaSyADjgRuNoE610_YLwW8P2_rKp08eEPDqFo')
+                self.save
+            end
+        rescue => exception
+        end
+    end
 end
